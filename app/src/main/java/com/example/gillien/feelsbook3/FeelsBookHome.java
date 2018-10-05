ef1d5f662e23cfa4b19717f0c820a9e9a0cca745
gfroilan <33307642+gfroilan@users.noreply.github.com>
2018-10-05 05:59:20 -0600

    Update FeelsBookHome.java

--- app/src/main/java/com/example/gillien/feelsbook3/FeelsBookHome.java
+++ app/src/main/java/com/example/gillien/feelsbook3/FeelsBookHome.java
@@ -1,5 +1,6 @@
 package com.example.gillien.feelsbook3;
 
+//import
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -7,16 +8,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//Home Page of FeelsBook app
 public class FeelsBookHome extends AppCompatActivity {
 
+    //class vars
     public static String EMOTION_INPUT;
 
     @Override
+    //Called when app icon is clicked on
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feels_book_home);
     }
 
+    //Handles when user enters love emotion
+    //Navigates to comment activity
     public void sendLove(View view) {
         Button btn = findViewById(R.id.button_love);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +36,8 @@ public class FeelsBookHome extends AppCompatActivity {
         });
     }
 
+    //Handles when user enters anger emotion
+    //Navigates to comment activity
     public void sendAnger(View view) {
         Button btn = findViewById(R.id.button_anger);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +51,8 @@ public class FeelsBookHome extends AppCompatActivity {
         });
     }
 
+    //Handles when user enters joy emotion
+    //Navigates to comment activity
     public void sendJoy(View view) {
         Button btn = findViewById(R.id.button_joy);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +66,8 @@ public class FeelsBookHome extends AppCompatActivity {
         });
     }
 
+    //Handles when user enters fear emotion
+    //Navigates to comment activity
     public void sendFear(View view) {
         Button btn = findViewById(R.id.button_fear);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -69,6 +81,8 @@ public class FeelsBookHome extends AppCompatActivity {
         });
     }
 
+    //Handles when user enters surprise emotion
+    //Navigates to comment activity
     public void sendSurprise(View view) {
         Button btn = findViewById(R.id.button_surprise);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +96,8 @@ public class FeelsBookHome extends AppCompatActivity {
         });
     }
 
+    //Handles when user enters sadness emotion
+    //Navigates to comment activity
     public void sendSadness(View view) {
         Button btn = findViewById(R.id.button_sadness);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -95,10 +111,13 @@ public class FeelsBookHome extends AppCompatActivity {
         });
     }
 
+    //Handles when user navigates to and from home page
     public static Intent makeHomeIntent(Context context) {
         return new Intent(context, FeelsBookHome.class);
     }
 
+    //Handles when user wants to view history
+    //Navigates to and from ViewHistory activity
     public void gotoDelete(View view) {
         Button btn = findViewById(R.id.button_history);
         btn.setOnClickListener(new View.OnClickListener() {
@@ -110,6 +129,8 @@ public class FeelsBookHome extends AppCompatActivity {
         });
     }
 
+    //Handles when user wants to view statistics
+    //Navigates to and from ViewStats activity
     public void gotoStats(View view) {
         Button btn = findViewById(R.id.button_stats);
         btn.setOnClickListener(new View.OnClickListener() {
